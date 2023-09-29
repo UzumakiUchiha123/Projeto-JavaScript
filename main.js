f485f6b2dfdfbf2fb7ed7c583a68c1d59c1a897d
Kevellyn.lima <110827364+UzumakiUchiha123@users.noreply.github.com>
2023-09-29 08:48:45 -0300

    2

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ while (contador < listaDeTeclas.length) {
      const instrumento = tecla.classList[1];
 
      const idAudio = '#som_${instrumento}';
+     
+     tecla.onclick = function (){
 
 
 
